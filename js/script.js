c5f9bb25d12d12c8ff51372c21cd842950b0b4d8
lesley_w <lrwesley92@gmail.com>
2021-08-03 12:08:21 -0500

    Refactored code a bit

--- js/script.js
+++ js/script.js
@@ -20,21 +20,39 @@ const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 //================================================
 
+//Adds validation of email when button is clicked
+
 button.addEventListener("click", function () {
-  const email = emailInput.value;
-
-  if (email.match(mailformat)) {
-    clearInput();
-    validateMessage.classList.add("hide");
-    emailDiv.classList.remove("active");
-  } else {
-    validateMessage.classList.remove("hide");
-    emailDiv.classList.add("active");
-  }
-
+  validateEmail();
 })
 
+//===============================================
+
+//Adds validation of email when "enter" is hit
+
+
+
+
 //==============================================
+
+ //Validates email
+
+ const validateEmail = function () {
+   const email = emailInput.value;
+
+   if (email.match(mailformat)) {
+     clearInput();
+     validateMessage.classList.add("hide");
+     emailDiv.classList.remove("active");
+   } else {
+     validateMessage.classList.remove("hide");
+     emailDiv.classList.add("active");
+   }
+ }
+
+//==============================================
+
+//Clears Input
 
 const clearInput = function () {
   emailInput.value = "";
